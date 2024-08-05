8becd360097f03bbb44f48a2428c00a7a7616c01
Herman-Gathege <siinamtechstudio@gmail.com>
2024-08-05 13:57:49 +0300

    first commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+function Article(props) {
+  return <h1>please pass this test</h1>;
 }
 
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
 export default Article;
